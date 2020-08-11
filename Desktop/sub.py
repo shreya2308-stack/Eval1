f2456b248206b15f375f84620ee20139f703ea9a
ShreyaChauhan <shreyachauhan2308@gmail.com>
2020-08-11 11:08:38 +0530

    first commit

---
+++ Desktop/sub.py
@@ -0,0 +1,12 @@
+a=int(input("please enter first number"))
+b=int(input("please enter second number"))
+def sub(a,b):
+  return a-b
+
+
+def multiply(a,b):
+   return a*b
+print(sub(a,b))
+print(multiply(a,b))
+
+  
